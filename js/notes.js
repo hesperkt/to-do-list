39b8e636d24ed606e08cd5e70af030f8ea6acb05
hesperkt <hk3@wellesley.edu>
2025-03-08 17:34:23 -0500

    added notes to separate file

---
+++ js/notes.js
@@ -0,0 +1,26 @@
+/*What can the user do
+What can they expect
+What do they see
+
+
+    user can add text to an input and click a button to submit task to list
+        add eventListener -> calls function addToList
+
+        function addToList
+            get the text from input
+            create li item
+            nb: create list element (ul or ol) in html
+            append li to list element
+            clear input
+    text is added to a list
+
+    See a list of things they add
+
+put their to-dos in an input
+create a button to add task
+    when button is clicked -> add/display task in to-do list
+display to-do list in a form/ul
+add delete button next to each task
+    when button is clicked -> remove task in to-do list
+add complete button that puts a line through a task when completed
+    when button is clicked -> task is complete and line is put through task*/
